39cee4072fcd302ccee816b7e4e91e8bf12db5e2
Riccardo Maio <riccardo.maio@monetate.com>
2026-06-02 09:50:26 -0400

    add setup_package in tests/__init__

---
+++ tests/__init__.py
@@ -0,0 +1,12 @@
+"""
+Package-level setup for the django-elasticache test suite.
+
+Configure Django settings before tests run.
+"""
+from django.conf import global_settings, settings
+
+
+def setup_package():
+    if not settings.configured:
+        settings.configure()
+        global_settings.configured = True
